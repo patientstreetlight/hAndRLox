34912d6888f7bf91382f6595536c11a237fd36a7
Chris Rice <chris@chrisrice.io>
2022-01-23 09:58:39 -0500

    Add Token type

--- hlox/src/Token.hs
+++ hlox/src/Token.hs
@@ -1,1 +1,51 @@
 module Token where
+
+
+data Token
+    -- Single-character tokens
+    = LeftParen
+    | RightParen
+    | LeftBrace
+    | RightBrace
+    | Comma
+    | Dot
+    | Minus
+    | Plus
+    | Semicolon
+    | Slash
+    | Star
+
+    -- One or two character tokens
+    | Bang
+    | BangEqual
+    | Equal
+    | EqualEqual
+    | Greater
+    | GreaterEqual
+    | Less
+    | LessEqual
+
+    -- Literals
+    | Identifier String
+    | String String
+    | Number Double
+
+    -- Keywords
+    | And
+    | Class
+    | Else
+    | TokFalse
+    | Fun
+    | For
+    | If
+    | Nil
+    | Or
+    | Print
+    | Return
+    | Super
+    | This
+    | TokTrue
+    | Var
+    | While
+
+    | Eof
