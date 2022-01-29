dadfeb4e35d4fb575bcb4d56e7a6a1b30261af76
Chris Rice <chris@chrisrice.io>
2022-01-29 12:23:17 -0500

    Main compiling

--- hlox/app/Main.hs
+++ hlox/app/Main.hs
@@ -1,5 +1,23 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.Exit (die)
 
 main :: IO ()
-main = putStrLn "hello"
+main = do
+    args <- getArgs
+    case args of
+        [] -> runPrompt
+        [fileName] -> runFile fileName
+        _ -> die "Usage: hlox [script]"
+
+
+runPrompt :: IO ()
+runPrompt = do
+    putStr "> "
+    -- XXX more here
+
+
+runFile :: String -> IO ()
+runFile fileName = do
+    putStrLn "running file "
